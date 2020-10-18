2dc292ae12739e2c20875fb3d1a1b0a56f5ed202
svenve-git <svenve@protonmail.com>
2020-10-18 10:26:22 +0200

    set up login page with input form & useMutation

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,33 +1,51 @@
 import React, { useState } from "react"
-import { View, Text, Image, TextInput, Button } from "react-native"
-
-/** TO DO
- * Add login query to the handler
- * Check if we get a token back
- * Add link to sign up page + text
- * Styling
- */
+import { View, Text, Image, TextInput, Button, StyleSheet } from "react-native"
+import { useMutation, gql } from "@apollo/client"
+import { LOGIN } from "../queries/queries"
 
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState("email")
-  const [password, setPassword] = useState("password")
+  const [email, setEmail] = useState("test@mail.com")
+  const [password, setPassword] = useState("123")
+  const [Login, { data }] = useMutation(LOGIN)
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      Login({ variables: { email: email, password: password } })
+      console.log("result:", data)
+    } catch (error) {
+      console.log("error:", error)
+    }
+    // CLEAR DATA FROM INPUT FIELDS?
+  }
 
   return (
-    <View>
-      <Text>Log In</Text>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        placeholder="email*"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        placeholder="password*"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Log in">Log in</Button>
+    <View style={styles.form}>
+      <Text style={styles.heading}>Log In</Text>
+      <Text>{email}</Text>
+      <Text>{password}</Text>
+      <View>
+        <Text>Enter email:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="example@mail.com"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+      </View>
+      <View>
+        <Text>Enter password:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="verystrongpassword"
+          label="Enter email:"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button title="Log in" onPress={handleLogin}>
+        Log in
+      </Button>
       <Text>
         No account?{" "}
         <Text
@@ -40,3 +58,23 @@ export default function LoginScreen({ navigation }) {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 7,
+  },
+  input: {
+    height: 40,
+    width: 250,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+})
