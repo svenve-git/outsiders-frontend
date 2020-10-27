bfb8496f463ba8e0411cb5a604c72aa4afe99456
svenve-git <svenve@protonmail.com>
2020-10-27 21:24:09 +0100

    fixed the Sign up screen placeholders & added some styling

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, Button, StyleSheet } from "react-native"
+import { View, StyleSheet, Pressable } from "react-native"
+import { Text, TextInput, Button } from "react-native-paper"
+import { FontAwesome5 } from "@expo/vector-icons"
 import { useMutation } from "@apollo/client"
 import { SIGNUP } from "../queries/queries"
 import AsyncStorage from "@react-native-community/async-storage"
-import { isSignedInVar } from "../cache"
+import { isSignedInVar } from "../config"
+
+/**
+ * To do:
+ * Add confirmation that account is made!
+ * And maybe a brief intro to the app
+ */
 
 export default function SignUpScreen({ navigation }) {
   /**
    *  Form state management
    */
-  const [fullName, setName] = useState("Marie Klaassen")
-  const [email, setEmail] = useState("marie@mail.nl")
-  const [password, setPassword] = useState("verystrongpassword")
-  const [address, setAddress] = useState("Damrak 3, 1012MB, Amsterdam")
-  const [gender, setGender] = useState("Female")
+  const [fullName, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [address, setAddress] = useState("")
+  const [gender, setGender] = useState("")
 
   /**
    *  App state management
@@ -54,35 +62,81 @@ export default function SignUpScreen({ navigation }) {
       <View style={styles.form}>
         <TextInput
           style={styles.input}
-          placeholder="name*"
+          placeholder="name"
           value={fullName}
           onChangeText={(text) => setName(text)}
         />
         <TextInput
           style={styles.input}
-          placeholder="email*"
+          placeholder="email"
           value={email}
           onChangeText={(text) => setEmail(text)}
         />
         <TextInput
           style={styles.input}
-          placeholder="password*"
+          placeholder="password"
           value={password}
           onChangeText={(text) => setPassword(text)}
         />
         <TextInput
           style={styles.input}
-          placeholder="address*"
+          placeholder="address"
           value={address}
           onChangeText={(text) => setAddress(text)}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="gender*"
-          value={gender}
-          onChangeText={(text) => setGender(text)}
-        />
-        <Button title="Sign up" onPress={handleSignUp}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            paddingHorizontal: 30,
+            marginTop: 20,
+            marginBottom: 20,
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              setGender("Female")
+              console.log("I'm pressed")
+            }}
+          >
+            <FontAwesome5
+              name="venus"
+              size={28}
+              color={gender === "Female" ? "#FF521B" : "black"}
+            />
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setGender("Male")
+            }}
+          >
+            <FontAwesome5
+              name="mars"
+              size={28}
+              color={gender === "Male" ? "#FF521B" : "black"}
+            />
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setGender("Transgender")
+            }}
+          >
+            <FontAwesome5
+              name="transgender-alt"
+              size={28}
+              color={gender === "Transgender" ? "#FF521B" : "black"}
+            />
+          </Pressable>
+        </View>
+        <Button
+          title="Sign up"
+          color={
+            fullName && email && password && address && gender
+              ? "#FF521B"
+              : "grey"
+          }
+          onPress={handleSignUp}
+        >
           Sign up
         </Button>
       </View>
@@ -112,8 +166,8 @@ const styles = StyleSheet.create({
   form: {
     justifyContent: "center",
     marginTop: 50,
-    padding: 20,
-    backgroundColor: "#ffffff",
+    // padding: 20,
+    // backgroundColor: "#ffffff",
   },
   input: {
     height: 40,
