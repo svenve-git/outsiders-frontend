27ca78ab11b5afae56be66743c901d38860b578f
svenve-git <svenve@protonmail.com>
2020-11-01 16:18:19 +0100

    added tab navigation w/ icons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,39 @@
 import React from "react"
-import { View, Text, Image, TextInput, Button } from "react-native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { FontAwesome5 } from "@expo/vector-icons"
+import MapScreen from "./Map"
+import ListScreen from "./List"
+import ProfileScreen from "./Profile"
 
-const HomeScreen = () => {
+const Tab = createBottomTabNavigator()
+
+export default function HomeScreen({ navigation, route }) {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Tab.Navigator
+      initialRouteName="Map"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === "Profile") {
+            iconName = "user"
+          } else if (route.name === "Map") {
+            iconName = "map-marked-alt"
+          } else if (route.name === "List") {
+            iconName = "list"
+          }
+          // You can return any component that you like here!
+          return <FontAwesome5 name={iconName} size={size} color={color} />
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "tomato",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="List" component={ListScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   )
 }
-
-export default HomeScreen
